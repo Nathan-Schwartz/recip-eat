9a2cf1d157e5eaa30c2769af6eb3f9c056e186f4
MKS-students <mks.students@hackreactor.com>
2016-03-24 16:45:48 -0500

    Two working endpoints for 2 apis

--- server/recipes.js
+++ server/recipes.js
@@ -3,12 +3,14 @@ var Promise = require('bluebird');
 
 var recipes = module.exports;
 
-recipes.get = function(options, ingredients, category, callback){
+recipes.searchRecipePuppy = function(options, ingredients, category, callback){
+
+  //check arguments
+  if(typeof ingredients !== "string" || typeof category !== "string" || typeof callback !== "function" || (typeof options !== "object" && options !== undefined))
+    throw new Error("recipes.searchRecipePuppy expects 1 options object, 2 strings, and 1 callback (respectively)");
+
 
   var allRecipes = [];
-
-  if(typeof ingredients !== "string" || typeof category !== "string" || typeof callback !== "function" || (typeof options !== "object" && options !== undefined))
-    throw new Error("recipes.get expects 1 options object, 2 strings, and 1 callback (respectively)")
 
   sendRequest(1)
   .then(function(body){
@@ -22,14 +24,25 @@ recipes.get = function(options, ingredients, category, callback){
   .then(function(body){
     return allRecipes.concat(JSON.parse(body).results);
   })
-  .then(function(allRecipes){
-    callback(allRecipes);
+
+  //filter if needed
+  .then(function(recipes){
+    if(options && options.filter === "thumbnail")
+      return recipes.filter(byThumbNail);
+
+    else
+      return recipes;
+  })
+
+  //give recipes back
+  .then(function(recipes){
+    callback(recipes);
   })
   .catch(function(error){
-    console.log("One of the page requests went poorly in recipes.js =>", error);
+    console.log("One of the page requests went poorly in searchRecipePuppy =>", error);
   })
 
-
+  //get all relevant recipes on the specified page
   function sendRequest(pageNum){
     return new Promise(function(resolve,reject){
       request('http://www.recipepuppy.com/api/?i=' + String(ingredients) + '&q=' + String(category) + '&p=' + String(pageNum), function (error, response, body){
@@ -42,3 +55,47 @@ recipes.get = function(options, ingredients, category, callback){
   }
 }
 
+recipes.searchFood2Fork = function(options, ingredients, callback){
+
+  //check arguments
+  if(typeof ingredients !== "string" || typeof callback !== "function" || (typeof options !== "object" && options !== undefined))
+    throw new Error("recipes.get expects 1 options object, 2 strings, and 1 callback (respectively)");
+
+  sendRequest(1)
+  .then(function(body){
+    return JSON.parse(body);
+  })
+
+  // //filter if needed
+  // .then(function(recipes){
+  //   if(options && options.filter === "thumbnail")
+  //     return recipes.filter(byThumbNail);
+
+  //   else
+  //     return recipes;
+  // })
+
+  //give recipes back
+  .then(function(recipes){
+    callback(recipes);
+  })
+  .catch(function(error){
+    console.log("The page request went poorly in searchFood2Fork =>", error);
+  })
+
+  //get all relevant recipes on the specified page
+  function sendRequest(pageNum){
+    return new Promise(function(resolve,reject){
+      request("http://food2fork.com/api/search?key=d4650ccd9d1218b3977c82811f4772ef&q=" + String(ingredients) + "&sort=r&page=" + String(pageNum), function (error, response, body){
+        if(error || response.statusCode !== 200)
+          reject(error);
+        else
+          resolve(body);
+      })
+    })
+  }
+}
+
+function byThumbNail(current) {
+  return current.thumbnail.length > 0;
+}
--- server/server.js
+++ server/server.js
@@ -7,19 +7,43 @@ var recipes = require('./recipes');
 
 var app = express();
 
-//var SERVER_KEY = ""
+var API_KEY = "d4650ccd9d1218b3977c82811f4772ef"
 var ROOT_DIRECTORY = __dirname.slice(0,__dirname.indexOf('/server'));
-
-console.log(ROOT_DIRECTORY);
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+//parse out all spaces
 
-recipes.get({}, "onions,garlic","omelet", function(recipes){
-  console.log("retrieved recipes:",recipes);
+
+app.post('/searchRecipePuppy', function(req, res) {
+
+//Is there a wildcard select for ingredients and category
+  var ingredients =  req.body.ingredients || "onions,garlic";
+  var category = req.body.category || "omelet";
+
+  recipes.searchRecipePuppy({}, ingredients, category, //to filter entries without picture pass in {filter:"thumbnail"} instead of {}
+    function(recipes){
+      console.log("retrieved recipes:",recipes);
+      res.send(JSON.stringify(recipes));
+    }
+  );
+});
+
+
+app.post('/searchFood2Fork', function(req, res) {
+
+//blank will get most popular things
+  var ingredients =  req.body.ingredients || "onions";
+
+  recipes.searchFood2Fork({}, ingredients, //to filter entries without picture pass in {filter:"thumbnail"} instead of {}
+    function(recipes){
+      console.log("retrieved recipes:",recipes);
+      res.send(JSON.stringify(recipes));
+    }
+  );
 });
 
 
