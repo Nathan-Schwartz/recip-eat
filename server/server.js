46909e5d15c299df78e1da5b4604e095ad5bca0c
MKS-students <mks.students@hackreactor.com>
2016-03-24 12:47:30 -0500

    Printing recipes

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var request = require('request');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 
@@ -10,17 +11,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '../public')); // idk if I can append '..'
 
+request('http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=1 ', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    processRecipes(JSON.parse(body).results);
+  } else {
+    if(error instanceof Error)
+      throw error;
+    else
+      console.log(error);
+  }
+})
 
-
-
-app.listen(4568);
-
-
-
-app.get()
-
-app.post()
-
+function processRecipes(recipes) {
+  console.log("recipes:",recipes)
+}
 
 app.listen(1337);
 console.log("App is listening");
