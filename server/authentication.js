2773ca1d053ea52aa688b31c687e956cac0a45b5
Nathan Schwartz <nathan.schwartz95@gmail.com>
2016-04-05 17:26:50 -0500

    Delete authentication.js

--- server/authentication.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  "Food2Plate": "d4650ccd9d1218b3977c82811f4772ef",
-  "gitHubClientSecret": "d6e017588fcb4f4773fff23fdc164d746eff9a77",
-  "githubClientId": "8562d9e68b5f5333368a"
-};
